5cf3568db90dff6db594d6b63b3b31cf763ef0a7
Luigi De Simone <luigi.desimone@unina.it>
2024-07-31 15:00:00 +0200

    update install_mongodb.sh for Debian 12

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -6,7 +6,8 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
    sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
    --dearmor
 
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+# DEBIAN 12 repo list
+echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/7.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
 sudo apt-get update
 
