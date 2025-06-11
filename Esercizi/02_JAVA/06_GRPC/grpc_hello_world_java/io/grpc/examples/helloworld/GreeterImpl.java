b290fad349d0e596b5eb78acba91f9a2c90c4e17
Luigi De Simone <luigi.desimone3@gmail.com>
2025-06-11 10:18:40 +0200

    update grpc java example and esercitazione STOMP-JMS-gRPC JAVA-PYTHON

---
+++ Esercizi/02_JAVA/06_GRPC/grpc_hello_world_java/io/grpc/examples/helloworld/GreeterImpl.java
@@ -0,0 +1,15 @@
+package io.grpc.examples.helloworld;
+
+import io.grpc.stub.StreamObserver;
+
+public class GreeterImpl extends GreeterGrpc.GreeterImplBase {
+    @Override
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        System.out.println("[SERVER JAVA GRPC] Invoked sayHello");
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+        System.out.println("[SERVER JAVA GRPC] Set reply and send...");
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
+    
